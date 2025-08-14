cf14bc7911c3975dc00e5853a979b350bcbe54fc
Hensemberk <juanchodve14@gmail.com>
2025-08-14 14:23:33 -0500

    Everything good

--- backend-nodejs-express/src/controllers/predictionController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import Note from '../models/Note'; // Import your Note model and interface
-import ApiError from '../errors/ApiError'; // Import your custom error class
-import { asyncHandler } from '../utils/asyncHandler'; // Import the asyncHandler utility
-
-export const getPrediction = asyncHandler(async (req:Request,res: Response, next:NextFunction) => {
-    const text = req.query.textPredict
-    
-})
